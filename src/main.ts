c5111910adaf1c8a52460a379906b57c040ab994
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-11-02 12:48:36 +0330

    modify addres for app.module and listen port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Start Server
+  // const { PORT } = process.env;
+  const PORT = 3000;
+  await app.listen(PORT, () => {
+    console.log(`Root:    http://localhost:${PORT}`);
+    // console.log(`Swagger: http://localhost:${PORT}/swagger`);
+  });
 }
 bootstrap();
