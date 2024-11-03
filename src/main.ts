1c998070e8c82fc90052e6a8b025d37a1b54c08d
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-11-03 11:11:42 +0330

    activate static folder and validation and cookies

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { SwaggerConfigInit } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Configure Swagger
+  SwaggerConfigInit(app);
+
+  // Static folder
+  app.useStaticAssets('public');
+
+  // Activate Validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Activate Cookie Parser
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+
   // Start Server
-  // const { PORT } = process.env;
-  const PORT = 3000;
+  const { PORT = 3000 } = process.env;
   await app.listen(PORT, () => {
     console.log(`Root:    http://localhost:${PORT}`);
-    // console.log(`Swagger: http://localhost:${PORT}/swagger`);
+    console.log(`Swagger: http://localhost:${PORT}/swagger`);
   });
 }
 bootstrap();
