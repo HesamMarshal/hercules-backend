a866c5bd2ec27ab9040dcf4bd3995f2586cfe853
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-04-11 19:53:11 +0330

    improve reading

--- src/common/typeDefinations/env.d.ts
+++ src/common/typeDefinations/env.d.ts
@@ -2,12 +2,14 @@ namespace NodeJS {
   interface ProcessEnv {
     //Application
     PORT: number;
+
     //Database
     DB_PORT: number;
     DB_NAME: string;
     DB_USERNAME: string;
     DB_PASSWORD: string;
     DB_HOST: string;
+
     //s3
     S3_ACCESS_KEY: string;
     S3_SECRET_KEY: string;
