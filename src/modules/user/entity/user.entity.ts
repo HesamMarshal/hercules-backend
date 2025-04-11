8de404d3547389c353f4e872cb252a6684884be2
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-04-11 19:54:29 +0330

    add user entity

---
+++ src/modules/user/entity/user.entity.ts
@@ -0,0 +1,59 @@
+import { EntityNames } from 'src/common/enum/entity-name.enum';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { MeasurementEntity } from './measurement.entity';
+// import { UserAddressEntity } from './address.entity';
+// import {OTPEntity} from "./otp.entity";
+
+@Entity(EntityNames.User)
+export class UserEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ nullable: true })
+  first_name: string;
+
+  @Column({ nullable: true })
+  last_name: string;
+
+  @Column({ unique: true })
+  mobile: string;
+  @Column({ nullable: true, default: false })
+  mobile_verify: boolean;
+
+  @Column({ nullable: true, unique: true })
+  email: string;
+
+  @Column({ unique: true, nullable: true })
+  invite_code: string;
+
+  @Column({ default: 0 })
+  score: number;
+
+  @Column({ nullable: true })
+  agentId: number;
+
+  // @CreateDateColumn({ type: 'time with time zone' })
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  // Relations
+  @OneToMany(() => MeasurementEntity, (measure) => measure.user)
+  measureList: MeasurementEntity[];
+
+  // @Column({ nullable: true })
+  // otpId: number;
+  // @OneToOne(() => OTPEntity, (otp) => otp.user)
+  // @JoinColumn()
+  // otp: OTPEntity;
+}
