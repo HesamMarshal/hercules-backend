aaad71a17cac05f30f4b0b4b813f6c1db0a71493
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-04-30 10:36:35 +0330

    add category Entity

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,1 +1,15 @@
-export class Category {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class CategoryEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @Column()
+  image: string;
+}
