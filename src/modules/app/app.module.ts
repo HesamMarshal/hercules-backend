e735a1111fbdcd1252e6079e1a2de0fce966a90c
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-04-30 10:40:17 +0330

    add category to app

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from 'src/config/typeorm.config';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig())],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig()), CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
