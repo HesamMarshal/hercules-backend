bd00895487fff4e4706b03ae24439ed9691c0813
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-04-30 10:38:04 +0330

    add dto

--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCategoryDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateCategoryDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  slug: string;
+
+  @ApiProperty({ format: 'binary' })
+  image: string;
+}
