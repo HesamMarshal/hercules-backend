c8e830a93227f684bea1f32bf21af29bd45f1123
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-05-02 13:32:25 +0330

    add Auth module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from 'src/config/typeorm.config';
 import { CategoryModule } from '../category/category.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig()), CategoryModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig()), AuthModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
