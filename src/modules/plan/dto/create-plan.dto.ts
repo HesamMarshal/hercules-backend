97bb5e375fca2ebd747b372a5f0efad58f784d9a
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-09 12:51:27 +0330

    add plan dto

--- src/modules/plan/dto/create-plan.dto.ts
+++ src/modules/plan/dto/create-plan.dto.ts
@@ -1,1 +1,9 @@
-export class CreatePlanDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreatePlanDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  order: number;
+}
