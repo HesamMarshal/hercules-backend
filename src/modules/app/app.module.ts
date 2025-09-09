2ceaaae5295ad89292e4ad134460d9992a7b170f
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-09 13:23:20 +0330

    add UserModule and Plan Module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -5,9 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from 'src/config/typeorm.config';
 import { CategoryModule } from '../category/category.module';
 import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
+import { PlanModule } from '../plan/plan.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig()), AuthModule, CategoryModule],
+  imports: [
+    TypeOrmModule.forRoot(TypeOrmConfig()),
+    AuthModule,
+    UserModule,
+    CategoryModule,
+    PlanModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
