96208fa89cfeaef5f155232178fb8e88039c60c2
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-09 12:38:28 +0330

    add some entities

--- src/common/enum/entity-name.enum.ts
+++ src/common/enum/entity-name.enum.ts
@@ -1,8 +1,24 @@
 export enum EntityNames {
   User = 'user',
   UserOtp = 'user_otp',
+  Role = 'role',
+  Permission = 'permission',
+  RolePermission = 'role_permission',
   Measurement = 'measurement',
+  MeasurementGoal = 'measurement_goal',
   UserAddress = 'user_address',
-  UserBasket = 'user_basket',
+
   Category = 'category',
+  Plan = 'plan',
+  Workout = 'workout',
+  Exercise = 'exercise',
+  ExerciseCategory = 'exercise_category',
+  ExerciseEquipment = 'exercise_equipment',
+  ExerciseBodyPart = 'exercise_body_part',
+  WorkoutExercise = 'workout_exercise',
+  SetType = 'set_type',
+  ExerciseType = 'exercise_type',
+
+  //Trainer = 'trainer',
+  //TrainerReview = 'trainer_review',
 }
