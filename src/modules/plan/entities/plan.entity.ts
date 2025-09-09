5cee3abb441949d358f6a9668478212188ae1b70
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-09 12:51:37 +0330

    add plan entity

--- src/modules/plan/entities/plan.entity.ts
+++ src/modules/plan/entities/plan.entity.ts
@@ -1,1 +1,33 @@
-export class Plan {}
+import { EntityNames } from 'src/common/enum/entity-name.enum';
+import { UserEntity } from 'src/modules/user/entity/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity(EntityNames.User)
+export class PlanEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ nullable: true })
+  name: string;
+
+  @Column({ nullable: true })
+  order: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  // Relations
+  @ManyToOne(() => UserEntity, (user) => user.id)
+  @JoinColumn()
+  user: UserEntity;
+}
