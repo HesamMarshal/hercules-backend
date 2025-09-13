67e2e44c41170b9aa746230ac1693c30c9dbd8af
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-13 10:35:07 +0330

    add imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { OTPEntity } from './entities/otp.entity';
 
 @Module({
+  imports: [AuthModule, TypeOrmModule.forFeature([UserEntity, OTPEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
