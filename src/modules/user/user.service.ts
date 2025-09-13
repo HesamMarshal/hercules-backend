b082733b740b4d5f6b530e7c4f0e28f1f5f1b3c5
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-13 11:08:30 +0330

    implement findAll method

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,13 +3,22 @@ import { Inject, Injectable, Scope } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable({ scope: Scope.REQUEST })
 export class UserService {
-  constructor(@Inject(REQUEST) private request: Request) {}
+  constructor(
+    @Inject(REQUEST) private request: Request,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
 
   findAll() {
-    return `This action returns all user`;
+    // This should work for admin only
+    const result = this.userRepository.find();
+    return result;
   }
 
   findOneById(id: number) {
