7602c6c72f02af33e8ca0f167ccb52173ae16633
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-13 10:34:19 +0330

    add user dto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty()
+  first_name: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  last_name: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  email: string;
+}
