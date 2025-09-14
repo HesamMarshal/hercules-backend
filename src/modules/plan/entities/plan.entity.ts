761b8d46b5a6c9aebfb75339c7b253969155b400
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-14 11:22:22 +0330

    fix issue: entity name to Plan

--- src/modules/plan/entities/plan.entity.ts
+++ src/modules/plan/entities/plan.entity.ts
@@ -11,7 +11,7 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity(EntityNames.User)
+@Entity(EntityNames.Plan)
 export class PlanEntity {
   @PrimaryGeneratedColumn('increment')
   id: number;
