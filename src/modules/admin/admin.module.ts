5dbf4eb3a1a409daffbfdeef528b6130ca766530
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-14 17:36:34 +0330

    add access to userEntity

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([])],
+  imports: [AuthModule, TypeOrmModule.forFeature([UserEntity])],
   controllers: [AdminController],
   providers: [AdminService],
 })
