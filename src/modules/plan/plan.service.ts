d7459225919ebcc5979bb00bd40b520ee2df2507
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-16 17:57:11 +0330

    implemented create a plan

--- src/modules/plan/plan.service.ts
+++ src/modules/plan/plan.service.ts
@@ -1,26 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { UpdatePlanDto } from './dto/update-plan.dto';
+import { UserService } from '../user/user.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PlanEntity } from './entities/plan.entity';
+import { Repository } from 'typeorm';
+import { PlanMessage } from 'src/common/messages/message.enum';
 
 @Injectable()
 export class PlanService {
-  create(createPlanDto: CreatePlanDto) {
-    return 'This action adds a new plan';
+  constructor(
+    private readonly userService: UserService,
+
+    @InjectRepository(PlanEntity)
+    private planRepository: Repository<PlanEntity>,
+  ) {}
+
+  async create(createPlanDto: CreatePlanDto) {
+    const user = await this.userService.findMyProfile();
+    const { name, order } = createPlanDto;
+
+    const plan = await this.planRepository.create({
+      user,
+      name,
+      order,
+    });
+    await this.planRepository.save(plan);
+
+    return {
+      message: PlanMessage.Created,
+    };
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all plan`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} plan`;
   }
 
-  update(id: number, updatePlanDto: UpdatePlanDto) {
+  async update(id: number, updatePlanDto: UpdatePlanDto) {
     return `This action updates a #${id} plan`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} plan`;
   }
 }
