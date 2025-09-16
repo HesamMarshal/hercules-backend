b641346c2304e31ef72903373a7126a98f5bc438
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-16 17:36:23 +0330

    add form type

--- src/modules/plan/plan.controller.ts
+++ src/modules/plan/plan.controller.ts
@@ -16,6 +16,7 @@ import { AuthGuard } from '../auth/guards/auth.guard';
 import { RoleGuard } from '../auth/guards/role.guard';
 import { Roles } from 'src/common/enum/role.enum';
 import { CanAccess } from 'src/common/decorators/role.decorator';
+import { FormType } from 'src/common/enum/form-type.enum';
 
 @Controller('plan')
 @ApiTags('Plan')
@@ -26,6 +27,7 @@ export class PlanController {
   constructor(private readonly planService: PlanService) {}
 
   @Post()
+  @ApiConsumes(FormType.Urlencoded)
   create(@Body() createPlanDto: CreatePlanDto) {
     return this.planService.create(createPlanDto);
   }
@@ -41,11 +43,13 @@ export class PlanController {
   }
 
   @Patch(':id')
+  @ApiConsumes(FormType.Urlencoded)
   update(@Param('id') id: string, @Body() updatePlanDto: UpdatePlanDto) {
     return this.planService.update(+id, updatePlanDto);
   }
 
   @Delete(':id')
+  @ApiConsumes(FormType.Urlencoded)
   remove(@Param('id') id: string) {
     return this.planService.remove(+id);
   }
