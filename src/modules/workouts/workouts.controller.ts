d3ce6f96ece211a4848ad17d17166bf3eabaeeef
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-19 17:08:56 +0330

    add Auth and form data type

--- src/modules/workouts/workouts.controller.ts
+++ src/modules/workouts/workouts.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { WorkoutsService } from './workouts.service';
 import { CreateWorkoutDto } from './dto/create-workout.dto';
 import { UpdateWorkoutDto } from './dto/update-workout.dto';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { Roles } from 'src/common/enum/role.enum';
+import { CanAccess } from 'src/common/decorators/role.decorator';
+import { FormType } from 'src/common/enum/form-type.enum';
 
 @Controller('workouts')
+@ApiTags('Workouts')
+@ApiBearerAuth('Authorization')
+@UseGuards(AuthGuard, RoleGuard) // Apply both guards
+@CanAccess(Roles.CLIENT, Roles.TRAINER, Roles.ADMIN)
 export class WorkoutsController {
   constructor(private readonly workoutsService: WorkoutsService) {}
 
   @Post()
+  @ApiConsumes(FormType.Urlencoded)
   create(@Body() createWorkoutDto: CreateWorkoutDto) {
     return this.workoutsService.create(createWorkoutDto);
   }
@@ -18,16 +38,19 @@ export class WorkoutsController {
   }
 
   @Get(':id')
+  @ApiConsumes(FormType.Urlencoded)
   findOne(@Param('id') id: string) {
     return this.workoutsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiConsumes(FormType.Urlencoded)
   update(@Param('id') id: string, @Body() updateWorkoutDto: UpdateWorkoutDto) {
     return this.workoutsService.update(+id, updateWorkoutDto);
   }
 
   @Delete(':id')
+  @ApiConsumes(FormType.Urlencoded)
   remove(@Param('id') id: string) {
     return this.workoutsService.remove(+id);
   }
