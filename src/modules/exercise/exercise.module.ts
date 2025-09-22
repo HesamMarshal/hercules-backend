c949fde12560ebda2dc05950137c34882ebb1f0f
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-22 13:43:56 +0330

    add auth and entity

--- src/modules/exercise/exercise.module.ts
+++ src/modules/exercise/exercise.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 import { ExerciseController } from './exercise.controller';
+import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExerciseEntity } from './entities/exercise.entity';
 
 @Module({
+  imports: [AuthModule, TypeOrmModule.forFeature([ExerciseEntity])],
   controllers: [ExerciseController],
   providers: [ExerciseService],
 })
