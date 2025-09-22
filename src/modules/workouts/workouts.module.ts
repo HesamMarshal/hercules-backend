292ec4d310f903705ac5b029be82e4bc5357afc4
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-22 12:35:36 +0330

    import plan service

--- src/modules/workouts/workouts.module.ts
+++ src/modules/workouts/workouts.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WorkoutsService } from './workouts.service';
 import { WorkoutsController } from './workouts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WorkoutEntity } from './entities/workout.entity';
+import { AuthModule } from '../auth/auth.module';
+import { PlanModule } from '../plan/plan.module';
 
 @Module({
+  imports: [AuthModule, PlanModule, TypeOrmModule.forFeature([WorkoutEntity])],
   controllers: [WorkoutsController],
   providers: [WorkoutsService],
 })
