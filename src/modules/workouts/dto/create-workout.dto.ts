6c9d49be1ffe99eff307b997cf957e69db3442f8
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-22 10:43:38 +0330

    add enum and dto

--- src/modules/workouts/dto/create-workout.dto.ts
+++ src/modules/workouts/dto/create-workout.dto.ts
@@ -1,1 +1,18 @@
-export class CreateWorkoutDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { WeekDays } from '../enums/weekDays.enum';
+import { IsEnum } from 'class-validator';
+
+export class CreateWorkoutDto {
+  @ApiProperty()
+  planId: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  order: number;
+
+  @ApiPropertyOptional({ nullable: true })
+  @IsEnum(WeekDays)
+  day_of_week: string;
+}
