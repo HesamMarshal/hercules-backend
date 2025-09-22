1db8337eca20388535cfd952ea4b4a5132b41d30
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-22 18:46:40 +0330

    add some validation

--- src/modules/workouts/dto/create-workout.dto.ts
+++ src/modules/workouts/dto/create-workout.dto.ts
@@ -1,18 +1,38 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { WeekDays } from '../enums/weekDays.enum';
-import { IsEnum } from 'class-validator';
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
+// TODO: Use Message enum
 export class CreateWorkoutDto {
   @ApiProperty()
+  @IsNotEmpty({ message: 'Plan ID is required' })
+  @Type(() => Number)
+  @IsInt({ message: 'Plan ID must be an integer' })
   planId: number;
 
   @ApiProperty()
+  @IsNotEmpty({ message: 'Workout name is required' })
+  @IsString({ message: 'Workout name must be a string' })
+  @MaxLength(100, {
+    message: 'Workout name must be shorter than or equal to 100 characters',
+  })
   name: string;
 
   @ApiPropertyOptional({ nullable: true })
+  @Type(() => Number)
+  @IsInt({ message: 'Order must be an integer' })
+  @Min(1, { message: 'Order must be at least 1' })
   order: number;
 
   @ApiPropertyOptional({ nullable: true })
-  @IsEnum(WeekDays)
+  @IsEnum(WeekDays, { message: 'Day of week must be a valid day' })
   day_of_week: string;
 }
