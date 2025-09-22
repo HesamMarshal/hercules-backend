2eeb061d6bbf008da57c954b3aa002e2d1c256cd
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-22 13:43:23 +0330

    add swagger decorators and Auth

--- src/modules/exercise/exercise.controller.ts
+++ src/modules/exercise/exercise.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 import { CreateExerciseDto } from './dto/create-exercise.dto';
 import { UpdateExerciseDto } from './dto/update-exercise.dto';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { CanAccess } from 'src/common/decorators/role.decorator';
+import { Roles } from 'src/common/enum/role.enum';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { FormType } from 'src/common/enum/form-type.enum';
 
 @Controller('exercise')
+@ApiTags('Exercise')
+@ApiBearerAuth('Authorization')
+@UseGuards(AuthGuard, RoleGuard) // Apply both guards
+@CanAccess(Roles.ADMIN)
 export class ExerciseController {
   constructor(private readonly exerciseService: ExerciseService) {}
 
   @Post()
+  @ApiConsumes(FormType.Urlencoded)
   create(@Body() createExerciseDto: CreateExerciseDto) {
     return this.exerciseService.create(createExerciseDto);
   }
@@ -18,16 +38,22 @@ export class ExerciseController {
   }
 
   @Get(':id')
+  @ApiConsumes(FormType.Urlencoded)
   findOne(@Param('id') id: string) {
     return this.exerciseService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateExerciseDto: UpdateExerciseDto) {
+  @ApiConsumes(FormType.Urlencoded)
+  update(
+    @Param('id') id: string,
+    @Body() updateExerciseDto: UpdateExerciseDto,
+  ) {
     return this.exerciseService.update(+id, updateExerciseDto);
   }
 
   @Delete(':id')
+  @ApiConsumes(FormType.Urlencoded)
   remove(@Param('id') id: string) {
     return this.exerciseService.remove(+id);
   }
