f319fac52b3452ed9d6c32b17e231e2e0644d4e2
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-22 12:36:40 +0330

    implement create and findAllById

--- src/modules/workouts/workouts.service.ts
+++ src/modules/workouts/workouts.service.ts
@@ -1,15 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateWorkoutDto } from './dto/create-workout.dto';
 import { UpdateWorkoutDto } from './dto/update-workout.dto';
-
+import { WorkoutEntity } from './entities/workout.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PlanService } from '../plan/plan.service';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
+import { AuthMessage } from 'src/common/messages/message.enum';
+import { WorkoutMessage } from './messages/message.enum';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { paginationSolver } from 'src/common/utility/pagination.util';
 @Injectable()
 export class WorkoutsService {
-  create(createWorkoutDto: CreateWorkoutDto) {
-    return 'This action adds a new workout';
+  constructor(
+    @Inject(REQUEST) private request: Request,
+    @InjectRepository(WorkoutEntity)
+    private workoutsRepository: Repository<WorkoutEntity>,
+
+    private readonly planService: PlanService,
+  ) {}
+  async create(createWorkoutDto: CreateWorkoutDto) {
+    const { user } = this?.request;
+    if (!user) throw new UnauthorizedException(AuthMessage.LoginAgain);
+
+    const { planId, name, order, day_of_week } = createWorkoutDto;
+
+    // check if the plan with give plan id is availabel
+    // check if the plan is belong to the current user?
+    // get the plan by plan id
+    const { data: plan } = await this.planService.findOne(+planId);
+
+    // create a new workout with date
+    const workouts = await this.workoutsRepository.create({
+      name,
+      order,
+      day_of_week,
+      plan,
+      createdBy: { id: user.id },
+    });
+
+    await this.workoutsRepository.save(workouts);
+
+    return { message: WorkoutMessage.Created };
   }
 
-  findAll() {
-    return `This action returns all workouts`;
+  async findAlByPlanId(planId: number, paginationDto: PaginationDto) {
+    const { user } = this?.request;
+    if (!user) throw new UnauthorizedException(AuthMessage.LoginAgain);
+    const { id } = user;
+    const { limit, page, skip } = paginationSolver(paginationDto);
+
+    // console.log(planId);
+    const { data: plan } = await this.planService.findOne(+planId);
+
+    // const result = await this.workoutsRepository.find();
+    // console.log(result);
+    const [workouts, total] = await this.workoutsRepository.findAndCount({
+      relations: {
+        plan: true, // This loads the plan relation
+      },
+      where: {
+        plan: {
+          id: planId, //  filter by relation field
+        },
+      },
+      select: {
+        plan: {
+          id: true,
+        },
+      },
+      order: {
+        order: 'DESC',
+        id: 'DESC',
+      },
+      skip, //: (page - 1) * limit,
+      take: limit,
+    });
+
+    //TODO: map workouts to remove some data
+    return {
+      data: workouts,
+      meta: {
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   }
 
   findOne(id: number) {
