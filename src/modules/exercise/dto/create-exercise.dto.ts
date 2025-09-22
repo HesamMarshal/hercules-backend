c2127daf1c11563df15023b1077aaed34710e1d3
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-22 13:43:40 +0330

    add entity and dto

--- src/modules/exercise/dto/create-exercise.dto.ts
+++ src/modules/exercise/dto/create-exercise.dto.ts
@@ -1,1 +1,37 @@
-export class CreateExerciseDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum } from 'class-validator';
+import { CategoryExercise } from '../enums/category.enum';
+import { BodyPart } from '../enums/bodyPart.enum';
+import { ExerciseType } from '../enums/exerciseType.enum';
+
+export class CreateExerciseDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  slug: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  @IsEnum(CategoryExercise)
+  category: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  @IsEnum(BodyPart)
+  body_part: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  @IsEnum(ExerciseType)
+  exercise_type: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  video_link: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  instruction: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  image: string;
+
+  @ApiPropertyOptional({ nullable: true })
+  image_key: string;
+}
--- src/modules/exercise/entities/exercise.entity.ts
+++ src/modules/exercise/entities/exercise.entity.ts
@@ -1,1 +1,59 @@
-export class Exercise {}
+import { EntityNames } from 'src/common/enum/entity-name.enum';
+
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { BodyPart } from '../enums/bodyPart.enum';
+import { CategoryExercise } from '../enums/category.enum';
+import { ExerciseType } from '../enums/exerciseType.enum';
+
+@Entity(EntityNames.Exercise)
+export class ExerciseEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  //  TODO: remove nullable
+  @Column({ nullable: true })
+  slug: string;
+
+  @Column({ type: 'enum', enum: CategoryExercise, nullable: true })
+  category: string;
+
+  @Column({ type: 'enum', enum: BodyPart, nullable: true })
+  body_part: string;
+
+  @Column({ type: 'enum', enum: ExerciseType, nullable: true })
+  exercise_type: string;
+
+  @Column({ nullable: true })
+  video_link: string;
+
+  @Column({ nullable: true })
+  instruction: string;
+
+  @Column({ nullable: true })
+  image: string;
+
+  @Column({ nullable: true })
+  image_key: string;
+
+  // Relations;
+  //   @ManyToOne(() => PlanEntity, (plan) => plan.id, { onDelete: 'CASCADE' })
+  //   @JoinColumn()
+  //   plan: PlanEntity;
+
+  //   @ManyToOne(() => UserEntity, (user) => user.id, {
+  //     onDelete: 'SET NULL',
+  //     nullable: true,
+  //   })
+  //   @JoinColumn({ name: 'created_by' })
+  //   createdBy: UserEntity;
+}
