81784970d1934c9f5077ace16aa983efac372c5d
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-23 12:25:59 +0330

    create message for Exercises

---
+++ src/modules/exercise/message/message.enum.ts
@@ -0,0 +1,7 @@
+export enum ExerciseMessage {
+  Created = 'حرکت ایجاد شد',
+  AlreadyExist = 'این حرکت وجود دارد',
+  NotFound = 'حرکت پیدا نشد',
+  Deleted = 'حرکت با موفقیت حذف گردید',
+  Updated = 'حرکت با موفقیت به روز رسانی گردید',
+}
