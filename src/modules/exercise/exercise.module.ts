dc4f9d7d7d822a0fac3af1fa1ea4f3f6b71a45c7
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-23 13:50:25 +0330

    add S3Service

--- src/modules/exercise/exercise.module.ts
+++ src/modules/exercise/exercise.module.ts
@@ -4,10 +4,11 @@ import { ExerciseController } from './exercise.controller';
 import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExerciseEntity } from './entities/exercise.entity';
+import { S3Service } from '../S3/s3.service';
 
 @Module({
   imports: [AuthModule, TypeOrmModule.forFeature([ExerciseEntity])],
   controllers: [ExerciseController],
-  providers: [ExerciseService],
+  providers: [ExerciseService, S3Service],
 })
 export class ExerciseModule {}
