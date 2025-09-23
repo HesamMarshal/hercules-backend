fd39916db4b7752410542774d4adad4cc21a1b32
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-23 12:29:53 +0330

    implement create, findAll, findone, findonebyslug

--- src/modules/exercise/exercise.service.ts
+++ src/modules/exercise/exercise.service.ts
@@ -1,26 +1,102 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateExerciseDto } from './dto/create-exercise.dto';
 import { UpdateExerciseDto } from './dto/update-exercise.dto';
+import { createSlug, randomId } from '../../common/utility/function.utils';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExerciseEntity } from './entities/exercise.entity';
+import { Repository } from 'typeorm';
+import { ExerciseMessage } from './message/message.enum';
+import { PaginationDto } from '../../common/dto/pagination.dto';
+import { paginationSolver } from '../../common/utility/pagination.util';
 
 @Injectable()
 export class ExerciseService {
-  create(createExerciseDto: CreateExerciseDto) {
-    return 'This action adds a new exercise';
+  constructor(
+    @InjectRepository(ExerciseEntity)
+    private exerciseRepository: Repository<ExerciseEntity>,
+  ) {}
+
+  async create(createExerciseDto: CreateExerciseDto) {
+    let {
+      name,
+      slug,
+      category,
+      body_part,
+      exercise_type,
+      video_link,
+      instruction,
+      image,
+      image_key,
+    } = createExerciseDto;
+
+    let slugDate = slug ?? name;
+    slug = createSlug(slugDate);
+
+    const isExist = await this.findOneBySlug(slug);
+    if (isExist) {
+      slug += `-${randomId()}`;
+    }
+
+    let exercise = this.exerciseRepository.create({
+      name,
+      slug,
+      category,
+      body_part,
+      exercise_type,
+      video_link,
+      instruction,
+      image,
+      image_key,
+    });
+    this.exerciseRepository.save(exercise);
+    return ExerciseMessage.Created;
   }
 
-  findAll() {
-    return `This action returns all exercise`;
+  async findAll(paginationDto: PaginationDto) {
+    const { limit, page, skip } = paginationSolver(paginationDto);
+
+    const [exercises, total] = await this.exerciseRepository.findAndCount({
+      order: {
+        name: 'DESC',
+        id: 'DESC',
+      },
+      skip, //: (page - 1) * limit,
+      take: limit,
+    });
+    return {
+      data: exercises,
+      meta: {
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} exercise`;
+  async findOne(id: number) {
+    const exercise = await this.exerciseRepository.findOneBy({ id });
+    if (!exercise) throw new NotFoundException(ExerciseMessage.NotFound);
+    return exercise;
+  }
+  async findOneBySlug(slug: string) {
+    const exercise = await this.exerciseRepository.findOneBy({ slug });
+    if (!exercise) throw new NotFoundException(ExerciseMessage.NotFound);
+    return exercise;
   }
 
-  update(id: number, updateExerciseDto: UpdateExerciseDto) {
+  async update(id: number, updateExerciseDto: UpdateExerciseDto) {
     return `This action updates a #${id} exercise`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} exercise`;
+  }
+
+  // Helper Functions
+
+  async checkBySlug(slug: string) {
+    const exercise = await this.exerciseRepository.findOneBy({ slug });
+    return exercise;
   }
 }
