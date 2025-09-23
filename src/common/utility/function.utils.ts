1fac401abe8629a2982e90b09001d00100a2d91a
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-23 12:25:34 +0330

    add randomId and createSlug

--- src/common/utility/function.utils.ts
+++ src/common/utility/function.utils.ts
@@ -1,10 +1,19 @@
 export function isBoolean(value: any) {
-  return ["false", false, "true", true].includes(value);
+  return ['false', false, 'true', true].includes(value);
 }
 export function toBoolean(value: any) {
-  return [true, "true"].includes(value)
+  return [true, 'true'].includes(value)
     ? true
-    : [false, "false"].includes(value)
+    : [false, 'false'].includes(value)
       ? false
       : value;
 }
+
+export const createSlug = (str: string) => {
+  return str
+    ?.replace(/[،:ًٌٍ\.\+\-_)(*&^%$#@!?;:><]+/g, '')
+    ?.replace(/[\s]+/g, '-')
+    .toLowerCase();
+};
+
+export const randomId = () => Math.random().toString(36).substring(2);
