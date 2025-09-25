763f226ef5789869aa28cdc40a2c23ee4cf1ca56
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-25 13:33:18 +0330

    add planEntity and export the module

--- src/modules/workouts/workouts.module.ts
+++ src/modules/workouts/workouts.module.ts
@@ -5,10 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkoutEntity } from './entities/workout.entity';
 import { AuthModule } from '../auth/auth.module';
 import { PlanModule } from '../plan/plan.module';
+import { PlanEntity } from '../plan/entities/plan.entity';
 
 @Module({
-  imports: [AuthModule, PlanModule, TypeOrmModule.forFeature([WorkoutEntity])],
+  imports: [
+    AuthModule,
+    PlanModule,
+    TypeOrmModule.forFeature([WorkoutEntity, PlanEntity]),
+  ],
   controllers: [WorkoutsController],
   providers: [WorkoutsService],
+  // TODO: If not used in other modules remove it
+  exports: [WorkoutsService],
 })
 export class WorkoutsModule {}
