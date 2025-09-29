81e7043e452d5fd13c80148c852f83149523c258
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-29 11:45:01 +0330

    use postgres variables

---
+++ src/database/data-source.ts
@@ -0,0 +1,24 @@
+// src/database/data-source.ts
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+config();
+
+const isProd = process.env.NODE_ENV === 'production';
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'herculesdb',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  synchronize: false, // IMPORTANT: false in dev too — use migrations
+  logging: !isProd,
+  // optional pool settings:
+  extra: {
+    max: +(process.env.DB_POOL_MAX || 10),
+  },
+});
