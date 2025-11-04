d7ce8578d41aae59777339b63281ee323480cb85
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-11-04 18:33:51 +0330

    remove factoryh

--- src/database/seeds/seed.ts
+++ src/database/seeds/seed.ts
@@ -1,9 +1,7 @@
 // src/database/seeds/seed.ts
-import { UserEntity } from 'src/modules/user/entities/user.entity';
 import dataSource from '../data-source';
 import { seedExercises } from './exercise.seed';
 import { seedPlan } from './plan.seed';
-import { runFactorySeed } from './seed.factory';
 import { seedUsers } from './user.seed';
 import { seedWorkouts } from './workout.seed';
 import { seedPractices } from './practice.seed';
