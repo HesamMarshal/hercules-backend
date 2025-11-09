df7eeb6f27567bb46c519d12a00d59cd65b957ff
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-11-09 16:24:19 +0330

    add  more messages

--- src/modules/session/messages/message.enum.ts
+++ src/modules/session/messages/message.enum.ts
@@ -7,4 +7,6 @@ export enum SessionMessage {
   SESSION_PRACTICE_NOT_FOUND = 'Session practice not found',
   CANNOT_RECORD_FOR_COMPLETED_SESSION = 'Cannot record sets for completed session',
   CANNOT_DELETE_OTHERS_SESSIONS = 'You can only delete your own sessions',
+  INVALID_SESSION_STATE_FOR_PAUSE = 'Session is not in a state that can be paused',
+  INVALID_SESSION_STATE_FOR_RESUME = 'Session is not in a state that can be resumed',
 }
