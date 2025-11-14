d3be597db33dc02769b022f290cd4a7cd5f912f8
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-11-14 13:19:51 +0330

    change integer to Number

--- src/common/decorators/pagination.decorator.ts
+++ src/common/decorators/pagination.decorator.ts
@@ -3,10 +3,10 @@ import { ApiQuery } from '@nestjs/swagger';
 
 export function Pagination(page: number = 1, limit: number = 10) {
   return applyDecorators(
-    ApiQuery({ name: 'page', type: 'integer', example: page, required: false }),
+    ApiQuery({ name: 'page', type: Number, example: page, required: false }),
     ApiQuery({
       name: 'limit',
-      type: 'integer',
+      type: Number,
       example: limit,
       required: false,
     }),
